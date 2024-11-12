89c790de211518ed300154903f06d4f505ce016d
Andreas-Holm-1 <andreas.ho.mat@gmail.com>
2024-11-12 15:21:36 +0100

    tset

---
+++ vgg16d.py
@@ -0,0 +1,338 @@
+import os
+import torch
+import PIL
+from torch import nn
+from torch.utils.data.dataloader import default_collate
+import pickle
+
+import torchvision.datasets as datasets
+from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+import torchvision.models
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from torchsummary import summary
+
+import utils
+
+# Check if you have cuda available, and use if you do
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+# Set a random seed for everything important
+def seed_everything(seed: int):
+    import random, os
+    import numpy as np
+    import torch
+    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = False
+
+# Set a seed with a random integer, in this case, I choose my verymost favourite sequence of numbers
+seed_everything(sum([115, 107, 105, 98, 105, 100, 105, 32, 116, 111, 105, 108, 101, 116]))
+
+# Specify dataset you wanna use
+def get_dataset(dataset_name, validation_size=0.1, transform=None, v=True):
+
+    if transform is None:
+        transform = ToTensor()
+
+    if dataset_name == 'cifar10':
+        train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=ToTensor())
+        test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=ToTensor())
+
+        # Purely for our convenience - Mapping from cifar labels to human readable classes
+        cifar10_classes = {
+            0: 'airplane',
+            1: 'automobile',
+            2: 'bird',
+            3: 'cat',
+            4: 'deer',
+            5: 'dog',
+            6: 'frog',
+            7: 'horse',
+            8: 'ship',
+            9: 'truck'
+        }
+
+    elif dataset_name == 'mnist':
+        train_set = datasets.MNIST(root='./data', train=True, download=True, transform=ToTensor())
+        test_set = datasets.MNIST(root='./data', train=False, download=True, transform=ToTensor())
+
+    elif dataset_name == 'imagenette':
+        download = not os.path.exists('./data/imagenette2')
+
+        # Specific transform in the case we use imagenette
+        imagenette_transform = transforms.Compose([
+            transforms.Resize(256),        # Resize to 256x256
+            transforms.RandomCrop(224),    # Crop the center to 224x224
+            transforms.ToTensor(),         # Convert to tensor
+            transforms.Normalize(mean=[0.4650, 0.4553, 0.4258], std=[0.2439, 0.2375, 0.2457]) # Normalize each image, numbers because of function courtesy of chatgpt
+        ])
+        train_set = datasets.Imagenette(root='./data', split='train', download=download, size='full', transform=imagenette_transform)
+        test_set = datasets.Imagenette(root='./data', split='val', download=False, size='full', transform=imagenette_transform)
+    
+    # If we want a validation set of a given size, take it from train set
+    if validation_size is not None:
+        # These will both be of the torch.utils.data.Subset type (not the Dataset type), and are basically just mappings of indices
+        # This does not matter when we make Dataloaders of them, however
+        if dataset_name != 'imagenette':
+            train_set, validation_set = torch.utils.data.random_split(train_set, [1-validation_size, validation_size])
+
+        # In the case of imagenette, the 'test set' is already a pretty big validation set, so we'll use that to create the test set instead
+        else:
+            validation_set, test_set = torch.utils.data.random_split(test_set, [validation_size, 1-validation_size])
+
+    if v:
+        print(f"There are {len(train_set)} examples in the training set")
+        print(f"There are {len(test_set)} examples in the test set \n")
+
+        print(f"Image shape is: {train_set[0][0].shape}, label example is {train_set[0][1]}")
+
+    return train_set, validation_set, test_set
+
+# collate function just to cast to device, same as in week_3 exercises
+def collate_fn(batch):
+    return tuple(x_.to(device) for x_ in default_collate(batch))
+
+
+
+
+# Get data
+dataset_name = 'imagenette'
+train_set, validation_set, test_set = get_dataset(dataset_name, validation_size=0.1)
+
+# Make dataloaders
+batch_size=16
+train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+validation_dataloader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+
+
+
+class VGG16(torch.nn.Module):
+    def __init__(self, num_classes, in_channels=1, features_fore_linear=25088, dataset=None):
+        super().__init__()
+        
+        # Helper hyperparameters to keep track of VGG16 architecture
+        conv_stride = ...
+        pool_stride = ...
+        conv_kernel = ...
+        pool_kernel = ...
+        dropout_probs = ...
+        optim_momentum = ...
+        weight_decay = ...
+        learning_rate = ...
+
+        # Define features and classifier each individually, this is how the VGG16-D model is orignally defined
+        # Define the VGG16-D convolutional (feature extraction) layers
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Flatten(),
+        )
+
+        # Define the VGG16-D fully connected (classification) layers
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+
+            nn.Linear(4096, num_classes),
+        )
+
+        
+        # In the paper, they mention updating towards the 'multinomial logistic regression objective'
+        # As can be read in Bishop p. 159, taking the logarithm of this equates to the cross-entropy loss function.
+        self.criterion = nn.CrossEntropyLoss()
+
+        # Optimizer - For now just set to Adam to test the implementation
+        self.optim = torch.optim.Adam(list(self.features.parameters()) + list(self.classifier.parameters()), lr=0.001)
+        # self.optim = torch.optim.SGD(list(self.features.parameters()) + list(self.classifier.parameters()), lr=learning_rate, momentum=optim_momentum, weight_decay=weight_decay)
+
+        self.dataset = dataset
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+    def train_model(self, train_dataloader, epochs=1, val_dataloader=None):
+        
+        # Call .train() on self to turn on dropout
+        self.train()
+
+        # To hold accuracy during training and testing
+        train_accs = []
+        test_accs = []
+
+        for epoch in range(epochs):
+            
+            epoch_acc = 0
+
+            for inputs, targets in tqdm(train_dataloader):
+                logits = self(inputs)
+                loss = self.criterion(logits, targets)
+                loss.backward()
+
+                self.optim.step()
+                self.optim.zero_grad()
+
+                # Keep track of training accuracy
+                epoch_acc += (torch.argmax(logits, dim=1) == targets).sum().item()
+            train_accs.append(epoch_acc / len(train_dataloader.dataset))
+
+            # If val_dataloader, evaluate after each epoch
+            if val_dataloader is not None:
+                # Turn off dropout for testing
+                self.eval()
+                acc = self.eval_model(val_dataloader)
+                test_accs.append(acc)
+                print(f"Epoch {epoch} validation accuracy: {acc}")
+                # turn on dropout after being done
+                self.train()
+        
+        return train_accs, test_accs
+
+    def eval_model(self, test_dataloader):
+        
+        self.eval()
+        total_acc = 0
+
+        for input_batch, label_batch in test_dataloader:
+            logits = self(input_batch)
+
+            total_acc += (torch.argmax(logits, dim=1) == label_batch).sum().item()
+
+        total_acc = total_acc / len(test_dataloader.dataset)
+
+        return total_acc
+
+    def predict(self, img_path):
+        img = PIL.Image.open(img_path)
+        img = self.dataset.dataset.transform(img)
+        classification = torch.argmax(self(img.unsqueeze(dim=0)), dim=1)
+        return img, classification
+    
+    def predict_random(self, num_predictions=16):
+        """
+        Plot random images from own given dataset
+        """
+        random_indices = np.random.choice(len(self.dataset)-1, num_predictions, replace=False)
+        classifcations = []
+        labels = []
+        images = []
+        for idx in random_indices:
+            img, label = self.dataset.__getitem__(idx)
+
+            classifcation = torch.argmax(self(img.unsqueeze(dim=0)), dim=1)
+
+            classifcations.append(classifcation)
+            labels.append(label)
+            images.append(img)
+
+        return classifcations, labels, images
+
+def get_vgg_weights(model):
+    """
+    Loads VGG16-D weights for the classifier to an already existing model
+    Also sets training to only the classifier
+    """
+    # Load the complete VGG16 model
+    temp = torchvision.models.vgg16(weights='DEFAULT')
+
+    # Get its state dict
+    state_dict = temp.state_dict()
+
+    # Change the last layer to fit our, smaller network
+    state_dict['classifier.6.weight'] = torch.randn(10, 4096)
+    state_dict['classifier.6.bias'] = torch.randn(10)
+
+    # Apply the state dict and set the classifer (layer part) to be the only thing we train
+    model.load_state_dict(state_dict)
+
+    for param in model.features.parameters():
+        param.requires_grad = False
+
+    model.optim = torch.optim.Adam(model.classifier.parameters())
+
+
+    return model
+
+
+
+
+in_channels = next(iter(train_dataloader))[0].shape[1]
+in_width_height = next(iter(train_dataloader))[0].shape[-1]
+# Make a dummy model to find out the size before the first linear layer
+CNN_model = VGG16(num_classes=10, in_channels=in_channels)
+
+# WARNING - THIS PART MIGHT BREAK
+features_fore_linear = utils.get_dim_before_first_linear(CNN_model.features, in_width_height, in_channels, brain=True)
+
+dummy_input = torch.randn(1, in_channels, in_width_height, in_width_height).to(device)
+dummy_output = CNN_model.features(dummy_input)
+n_features = dummy_output.shape[1]
+
+
+# Initialize model
+CNN_model = VGG16(num_classes=10, in_channels=in_channels, features_fore_linear=n_features, dataset=test_set)
+
+# Load VGG16 pre-trained weights
+CNN_model = get_vgg_weights(CNN_model)
+
+# Train model
+train_epochs = 5
+train_accs, test_accs = CNN_model.train_model(train_dataloader, epochs=train_epochs, val_dataloader=test_dataloader)
+
+with open('train_accs.pkl', 'wb') as f:
+    pickle.dump(train_accs, f)
+
+with open('test_accs.pkl', 'wb') as f:
+    pickle.dump(test_accs, f)
